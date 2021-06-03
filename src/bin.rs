d7b13ff1bb85e69b831c989aa3756aeb3469c8ea
Rahul Garg <rg@vihu.dev>
2021-06-03 16:51:27 -0700

    Invoke angry_purple_tiger from CLI
    - Add bin support
    - Add clap dependency

---
+++ src/bin.rs
@@ -0,0 +1,18 @@
+#[macro_use]
+extern crate clap;
+
+fn main() {
+    let matches = clap_app!(myapp =>
+        (version: "1.0")
+        (about: "Convert B58 address to animal name")
+        (@arg ADDR: +required "The address to convert")
+    )
+    .get_matches();
+
+    if let Some(addr) = matches.value_of("ADDR") {
+        let animal_name = addr
+            .parse::<angry_purple_tiger::AnimalName>()
+            .expect("animal name");
+        println!("{}", animal_name);
+    }
+}
